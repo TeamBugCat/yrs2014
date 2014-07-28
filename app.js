ee9b2a337240affe52a4d07595df9c886aaf042e
Matt Randell <ranmat11@gmail.com>
2014-07-28 15:55:48 +0100

    Really this time...

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var http = require('http');
 var app = express();
 
 // Set Main App Variables
-app.set('port', 4000 || process.env.PORT);
+app.set('port', process.env.PORT4000 || 4000);
 
 //Set Server up
 // Start App requests

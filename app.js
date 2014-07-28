1cc2d87ea13aa990b9e83b0cf54c3f6375d964b5
Matt Randell <ranmat11@gmail.com>
2014-07-28 16:08:37 +0100

    Added space for api

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('port', process.env.PORT || 4000);
 app.use('/',express.static(__dirname + "/build/web"));
 app.use(harp.mount(__dirname + "/build/web"));
 
+/* Insert api defs here */
+
 // Launch
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));

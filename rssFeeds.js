ff5cbdc4c53e7a258bc3fecbc849bfbe3bbcb7d6
Matt Randell <ranmat11@gmail.com>
2014-07-30 11:13:39 +0100

    Logo data

--- rssFeeds.js
+++ rssFeeds.js
@@ -1,6 +1,7 @@
 module.exports={
 	bbcNews:{
 		index:"http://feeds.bbci.co.uk/news/rss.xml",
-		namePretty: "BBC News"
+		namePretty: "BBC News",
+		logo: "http://news.bbc.co.uk/nol/shared/spl/hi/uk/08/broad_band_britain/swf/download.jpg?anticache=0.48376251850277185"
 	}
 }

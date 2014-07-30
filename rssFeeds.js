a0dad4188876f6ef56f5049f3901d728fad827c5
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-07-30 14:19:46 +0100

    Update rssFeeds.js

--- rssFeeds.js
+++ rssFeeds.js
@@ -4,5 +4,11 @@ module.exports={
 		index:"http://feeds.bbci.co.uk/news/rss.xml",
 		namePretty: "BBC News",
 		logo: "http://news.bbc.co.uk/nol/shared/spl/hi/uk/08/broad_band_britain/swf/download.jpg?anticache=0.48376251850277185"
+	},
+	theGuardian:{
+		id: "theGuardian",
+		index: "http://www.theguardian.com/uk/rss",
+		namePretty: "The Guardian",
+		logo: ""
 	}
 }
